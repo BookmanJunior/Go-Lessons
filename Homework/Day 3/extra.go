53dd467b3c0b29dfe35ba3fc5094e0998250616e
Gulov Azam <gulovazam@gmail.com>
2024-07-14 01:01:51 +0500

    Refactor GenerateInvoiceNumber
    add padNumber function

--- Homework/Day 3/extra.go
+++ Homework/Day 3/extra.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -20,20 +21,24 @@ func GeneratePassword() string {
 
 func GenerateInvoiceNumber() {
 	currentDate := time.Now()
-	currentOrder := 100
-	var orderFormatted string
-
-	if currentOrder < 10 {
-		orderFormatted = "00" + strconv.Itoa(currentOrder)
-	} else if currentOrder < 100 {
-		orderFormatted = "0" + strconv.Itoa(currentOrder)
-	} else {
-		orderFormatted = strconv.Itoa(currentOrder)
-	}
-	fmt.Printf("INV-%v-0%v-%v\n", currentDate.Year(), int(currentDate.Month()), orderFormatted)
+	formattedMonth := strings.Split(currentDate.Format(time.RFC3339), "-")[1]
+	currentOrder := 1
+	formattedOrder := padNumber(currentOrder)
+	fmt.Printf("INV-%v-%v-%v\n", currentDate.Year(), formattedMonth, formattedOrder)
 }
 
 func FormatDate() {
 	currentDate := time.Now()
 	fmt.Printf("%v %v %v\n", currentDate.Day(), currentDate.Month(), currentDate.Year())
+}
+
+func padNumber(num int) string {
+	numToString := strconv.Itoa(num)
+	if num < 10 {
+		return "00" + numToString
+	} else if num < 100 {
+		return "0" + numToString
+	} else {
+		return numToString
+	}
 }
