a1df2a58a3d0ed0528a14c8a86ae8fc785607475
Gulov Azam <gulovazam@gmail.com>
2024-07-14 01:22:13 +0500

    Add and comment out functions
    Add Pow4 function

--- Homework/Day 3/main.go
+++ Homework/Day 3/main.go
@@ -1,17 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"math"
+)
 
 func main() {
-	result := Calculate(5, 10, adder)
-	fmt.Println(result)
-	DisplaySeprator()
-	multiplier := Multiplier(5)
-	fmt.Println(multiplier(10))
-	DisplaySeprator()
-	fmt.Println(GeneratePassword())
-	DisplaySeprator()
+	// PrintGreeting()
+	// DisplaySeparator()
+	// NotifyStart()
+	// PrintNumber(50)
+	// GreetUser("Azam")
+	// ToggleLight(true)
+	// fmt.Println(adder(5, 5))
+	// fmt.Println(concatenator("Hello ", "World"))
+	// fmt.Println(isPositive(10))
+	// fmt.Println(isServiceActive())
+	// fmt.Println(isEven(9))
+	// result := Calculate(5, 10, adder)
+	// fmt.Println(result)
+	// multiplier := Multiplier(5)
+	// fmt.Println(multiplier(10))
+	// stringRepeater := StringRepeater(5)
+	// fmt.Println(stringRepeater("Hello"))
+	// conditionalRepeater := ConditionalRepeater(true)
+	// conditionalRepeater(":)")
+	// Execute(false, ToggleLight)
+	// fmt.Println(Apply(2, Pow4))
+	// fmt.Println(GeneratePassword())
 	GenerateInvoiceNumber()
-	DisplaySeprator()
-	FormatDate()
+	// FormatDate()
+}
+
+func Pow4(num int) int {
+	return int(math.Pow(float64(num), 4))
 }
